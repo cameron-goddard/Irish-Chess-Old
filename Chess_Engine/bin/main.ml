f78a051ea5772d9458f40c1ea4f2269d13544241
Christopher Price <csp82@cornell.edu>
2022-10-15 15:13:03 -0400

    woahhhhh things worky
    --if you do make run
    --you'll see stuf ...... woahhh

--- Chess_Engine/bin/main.ml
+++ Chess_Engine/bin/main.ml
@@ -1,10 +1,14 @@
 open Chess
-open Graphics
+(* open Graphics *)
 
 (* let _ = open_graph ""
 let _ = set_color blue
 let _ = fill_rect 0 0 100 100 *)
 
+
+
 let () =
-  open_graph "";
+  Graphics.open_graph "";
+  Graphics.fill_rect 0 0 100 100;
+  Unix.sleep 10;
   print_string "Hi";
