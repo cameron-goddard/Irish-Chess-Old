5bae56aee52aa47fb23d9db028b6bfbb91e44935
Cameron Goddard <camerongoddard@me.com>
2022-10-29 00:35:18 -0400

    restructure files

---
+++ bin/main.ml
@@ -0,0 +1,7 @@
+open Chess.Tile
+open Chess.Draw_piece
+open Graphics
+open Chess.Board_text
+
+let main () = Chess.Controller.main ()
+let () = main ()
--- src/author.mli
+++ src/author.mli
@@ -1,5 +1,5 @@
 (* CS 3110 Fall 2022 Final Project
-    @author csp82, *)
+    @author csp82, app67, ssm237, csg83 *)
 
 val hours_worked : int 
 (* [hours_worked] is the culmulative number of hours spent by all authors *)
